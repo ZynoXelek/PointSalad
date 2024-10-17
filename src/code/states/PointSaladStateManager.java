c9b2d5a1b628c24ea3511aa5153569b305b9e506
ZynoXelek <enzo.brossier-secher@student.isae-supaero.fr>
2024-10-17 23:36:54 +0200

    Add interface and implementation of PhaseManager

---
+++ src/code/states/PointSaladStateManager.java
@@ -0,0 +1,42 @@
+package code.states;
+
+import code.exceptions.PhaseException;
+import code.phases.IPhase;
+
+/**
+ * Manages the state of the game for the Point Salad game.
+ */
+public class PointSaladStateManager implements IStateManager {
+
+	private State gameState;
+
+	/**
+	 * Initializes the state manager with the given game state.
+	 * 
+	 * @param gameState The game state to initialize the manager with.
+	 */
+	public PointSaladStateManager(State gameState) {
+		this.gameState = gameState;
+	}
+
+	@Override
+	public void setState(State gameState) {
+		this.gameState = gameState;
+	}
+
+	@Override
+	public State getState() {
+		return gameState;
+	}
+
+	@Override
+	public void update() throws PhaseException {
+		boolean finished = false;
+
+		while (!finished) {
+			IPhase phase = gameState.getPhase();
+			phase.processPhase(gameState);
+			finished = !phase.proceedToNextPhase(gameState);
+		}
+	}
+}
