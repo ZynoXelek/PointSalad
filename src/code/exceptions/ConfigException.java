9ec3bf62558ca405f613b360a76568a5e5689f81
ZynoXelek <enzo.brossier-secher@student.isae-supaero.fr>
2024-10-17 23:37:20 +0200

    Add config class, exception and file

---
+++ src/code/exceptions/ConfigException.java
@@ -0,0 +1,33 @@
+package code.exceptions;
+
+/**
+ * Exception for when there is an error with the configuration.
+ */
+public class ConfigException extends Exception {
+	
+	/**
+	 * Creates a new ConfigException.
+	 */
+	public ConfigException() {
+		super();
+	}
+
+	/**
+	 * Creates a new ConfigException with the specified message.
+	 * 
+	 * @param message
+	 */
+	public ConfigException(String message) {
+		super(message);
+	}
+
+	/**
+	 * Creates a new ConfigException with the specified message and cause.
+	 * 
+	 * @param message The message.
+	 * @param cause The cause.
+	 */
+	public ConfigException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
