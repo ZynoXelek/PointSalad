f46bfac233bf40bf797de8e4c40c7b46cee4cc9f
ZynoXelek <enzo.brossier-secher@student.isae-supaero.fr>
2024-10-18 02:54:35 +0200

    Add Client main

---
+++ src/code/main/AbstractClient.java
@@ -0,0 +1,187 @@
+package code.main;
+
+import code.exceptions.ClientException;
+import code.network.IClientConnection;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+/**
+ * Abstract class for a client.
+ */
+public abstract class AbstractClient {
+	
+	private IClientConnection connection;
+	private Scanner scanner;
+
+	/**
+	 * Creates a client with the given connection.
+	 * 
+	 * @param connection The connection of the client
+	 * 
+	 * @throws ClientException If an error occurs while creating or processing the client
+	 */
+	public AbstractClient(IClientConnection connection) throws ClientException {
+		this.connection = connection;
+		this.scanner = new Scanner(System.in);
+
+		connect();
+
+		play();
+
+		quit();
+	}
+
+	/**
+	 * Gets the connection of the client.
+	 * 
+	 * @return The connection of the client
+	 */
+	public IClientConnection getConnection() {
+		return connection;
+	}
+
+	/**
+	 * Sets the connection of the client.
+	 * 
+	 * @param connection The connection of the client
+	 */
+	public void setConnection(IClientConnection connection) {
+		this.connection = connection;
+	}
+
+	/**
+	 * Gets the scanner of the client.
+	 * 
+	 * @return The scanner of the client
+	 */
+	public Scanner getScanner() {
+		return scanner;
+	}
+
+	/**
+	 * Sets the scanner of the client.
+	 * 
+	 * @param scanner The scanner of the client
+	 */
+	public void setScanner(Scanner scanner) {
+		this.scanner = scanner;
+	}
+
+	/**
+	 * Connects the client.
+	 * 
+	 * @throws ClientException If an error occurs while connecting the client
+	 */
+	public void connect() throws ClientException {
+		System.out.println("Connecting to the host...");
+		connection.connect();
+		System.out.println("Connected to the host.");
+	}
+
+	/**
+	 * Plays the game hosted on the server.
+	 * 
+	 * @throws ClientException If an error occurs while playing
+	 */
+	public abstract void play() throws ClientException;
+
+	/**
+	 * Quits the game and stops the client.
+	 * 
+	 * @throws ClientException If an error occurs while stopping the client
+	 */
+	public void quit() throws ClientException {
+		System.out.println("Stopping the client...");
+		connection.disconnect();
+		scanner.close();
+		System.out.println("Client stopped.");
+
+		System.exit(0);
+	}
+
+	/**
+	 * Checks if a host is valid.
+	 * 
+	 * @param host The host to check
+	 * 
+	 * @return True if the host is valid, false otherwise
+	 */
+	public static boolean isHostValid(String host) {
+		String ipPartRegex = "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)";
+		String hostRegex = "^(" + ipPartRegex + "\\.){3}" + ipPartRegex + "$";
+		Pattern pattern = Pattern.compile(hostRegex);
+		Matcher matcher = pattern.matcher(host);
+		return matcher.matches();
+	}
+
+	/**
+	 * Checks if a port is valid.
+	 * 
+	 * @param port The port to check
+	 * 
+	 * @return True if the port is valid, false otherwise
+	 */
+	public static boolean isPortValid(int port) {
+		return port >= 0 && port <= 65535;
+	}
+	
+	/**
+	 * Gets a valid host String from the terminal.
+	 * 
+	 * @return A valid host String from the terminal
+	 */
+	public static String getValidHostFromTerminal() {
+		Scanner scanner = new Scanner(System.in);
+		String host = null;
+		boolean isValid = false;
+
+		while (!isValid) {
+			System.out.print("Enter the host of the server: ");
+			host = scanner.nextLine();
+			
+			if (host == null || host.isEmpty()) {
+				System.out.println("The host cannot be empty.");
+			} else if (!isHostValid(host)) {
+				System.out.println("The host must be a valid IPv4 address, meaning it must be in the form of X.X.X.X where each X is between 0 and 255.");
+			} else {
+				isValid = true;
+			}
+		}
+		
+		scanner.close();
+		return host;
+	}
+
+	/**
+	 * Gets a valid port from the terminal.
+	 * 
+	 * @return A valid port from the terminal
+	 */
+	public static int getValidPortFromTerminal() {
+		Scanner scanner = new Scanner(System.in);
+		int port = 0;
+		boolean isValid = false;
+		
+		while (!isValid) {
+			System.out.print("Enter the port of the server: ");
+			String portString = scanner.nextLine();
+			
+			try {
+				port = Integer.parseInt(portString);
+				
+				if (!isPortValid(port)) {
+					System.out.println("The port must be between 0 and 65535.");
+				} else {
+					isValid = true;
+				}
+			} catch(NumberFormatException e) {
+				System.out.println("The port must be an integer.");
+			}
+		}
+		
+		scanner.close();
+		return port;
+	}
+}
---
+++ src/code/main/PointSaladClient.java
@@ -0,0 +1,60 @@
+package code.main;
+
+import code.exceptions.ClientException;
+import code.network.ClientConnection;
+
+/**
+ * This class is the main class on the player side to join a PointSalad game as a Client and play the game.
+ */
+public class PointSaladClient extends AbstractClient {
+
+	/**
+	 * Creates a PointSalad client with the given host and port.
+	 * 
+	 * @param host The host of the server
+	 * @param port The port of the server
+	 * 
+	 * @throws ClientException If an error occurs while creating or processing the client
+	 */
+	public PointSaladClient(String host, int port) throws ClientException {
+		super(new ClientConnection(host, port));
+	}
+
+	/**
+	 * Creates a PointSalad client from the terminal.
+	 * 
+	 * @return A PointSalad client created from the terminal
+	 * 
+	 * @throws ClientException If an error occurs while creating or processing the client
+	 */
+	public static PointSaladClient createFromTerminal() throws ClientException {
+		String host = getValidHostFromTerminal();
+		int port = getValidPortFromTerminal();
+		
+		return new PointSaladClient(host, port);
+	}
+
+	/**
+	 * Process the client side of the game.
+	 * For the PointSalad game, the client keeps receiving messages until the game ends.
+	 * The final received message is the only one containing the keyword "winner".
+	 * Messages to which the client must answer are the ones containing the keyword "Please".
+	 * 
+	 * @throws ClientException If an error occurs while processing the client
+	 */
+	@Override
+	public void play() throws ClientException {
+		ClientConnection connection = (ClientConnection) getConnection();
+		String receivedMessage = "";
+		String answer = "";
+
+		while (!receivedMessage.contains("winner")) {
+			receivedMessage = connection.readMessage();
+			System.out.println(receivedMessage);
+			if (receivedMessage.contains("Please")) {
+				answer = getScanner().nextLine();
+				connection.sendMessage(answer);
+			}
+		}
+	}
+}
